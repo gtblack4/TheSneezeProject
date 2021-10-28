bb86d5e38aa84d24f004638c9798285f1e704036
Gage Black <gtblack4@gmail.com>
2021-10-28 18:00:19 -0400

    forgot gunicorn

--- UpdateCSV.py
+++ UpdateCSV.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def updateSpeadsheet():
-	if os.getenv['ENVIRON'] == "PROD":
+	if os.getenv('ENVIRON') == "PROD":
 		gc = gspread.service_account_from_dict(os.getenv('CREDS'))
 	else:
 		gc = gspread.service_account(filename='service_account.json')
